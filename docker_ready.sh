778ae75c54f28a8e3fcc74ffc302e457e1281f26
bhctsntrk <behcet.senturk@protonmail.com>
2020-01-06 21:13:14 +0300

    Initial commit as ROS package. QLearning ready for turtbt3

---
+++ docker_ready.sh
@@ -0,0 +1,23 @@
+apt update && \
+source /opt/ros/melodic/setup.bash && \
+apt install vim \
+tmux \
+bash-completion \
+git \
+htop \
+python3-yaml \
+ros-melodic-rosserial-python \
+ros-melodic-rosserial-client \
+ros-melodic-xacro \
+ros-melodic-joy \
+ros-melodic-hardware-interface \
+ros-melodic-ros-control \
+ros-melodic-controller-manager \
+ros-melodic-gazebo-ros-control \
+ros-melodic-joint-state-controller \
+ros-melodic-effort-controllers \
+ros-melodic-position-controllers \
+ros-melodic-diff-drive-controller \
+"ros-melodic-turtlebot3*" \
+python3-pip && \
+pip3 install keras tensorflow matplotlib gym rospkg catkin_pkg defusedxml
---
+++ src/gazebo_turtlebot3_dqlearn.py
@@ -0,0 +1,112 @@
+import rospy
+import roslaunch
+import time
+import numpy as np
+
+from gym import spaces
+from geometry_msgs.msg import Twist
+from std_srvs.srv import Empty
+
+from sensor_msgs.msg import LaserScan
+
+from gym.utils import seeding
+
+class GazeboTurtlebot3DQLearnEnv():
+
+    def __init__(self):
+        # Launch the simulation with the given launchfile name
+        rospy.init_node('gazebo_dqlearning_node', anonymous=True)
+        self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+        self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+        self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
+        self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+
+        self.reward_range = (-np.inf, np.inf)
+
+        self.seed()
+
+    def calculate_observation(self,data):
+        min_range = 0.2
+        done = False
+        for i, item in enumerate(data.ranges):
+            if (min_range > data.ranges[i] > 0):
+                done = True
+        return data.ranges,done
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def step(self, action):
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        try:
+            self.unpause()
+        except Exception:
+            print ("/gazebo/unpause_physics service call failed")
+
+        max_ang_speed = 0.3
+        ang_vel = (action-10)*max_ang_speed*0.1 #from (-0.33 to + 0.33)
+
+        vel_cmd = Twist()
+        vel_cmd.linear.x = 0.2
+        vel_cmd.angular.z = ang_vel
+        self.vel_pub.publish(vel_cmd)
+        data = None
+        while data is None:
+            try:
+                data = rospy.wait_for_message('/scan', LaserScan, timeout=5)
+            except Exception:
+                print("/scan Error laser data is empty!")
+                time.sleep(5)
+
+        rospy.wait_for_service('/gazebo/pause_physics')
+        try:
+            #resp_pause = pause.call()
+            self.pause()
+        except Exception:
+            print ("/gazebo/pause_physics service call failed")
+
+        state,done = self.calculate_observation(data)
+
+        if not done:
+            reward = round(15*(max_ang_speed - abs(ang_vel) +0.0335), 2)
+        else:
+            reward = -200
+
+        return np.asarray(state), reward, done, {}
+
+    def reset(self):
+        # Resets the state of the environment and returns an initial observation.
+        rospy.wait_for_service('/gazebo/reset_simulation')
+        try:
+            #reset_proxy.call()
+            self.reset_proxy()
+        except Exception:
+            print ("/gazebo/reset_simulation service call failed")
+
+        # Unpause simulation to make observation
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        try:
+            #resp_pause = pause.call()
+            self.unpause()
+        except Exception:
+            print ("/gazebo/unpause_physics service call failed")
+        #read laser data
+        data = None
+        while data is None:
+            try:
+                data = rospy.wait_for_message('/scan', LaserScan, timeout=5)
+            except Exception:
+                print("/scan Error laser data is empty!")
+                time.sleep(5)
+
+        rospy.wait_for_service('/gazebo/pause_physics')
+        try:
+            #resp_pause = pause.call()
+            self.pause()
+        except Exception:
+            print ("/gazebo/pause_physics service call failed")
+
+        state,done = self.calculate_observation(data)
+
+        return np.asarray(state)
