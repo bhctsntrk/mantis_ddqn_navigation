e3479b887e13fb6ba87a562f48ef1b3c7b5e60f9
bhctsntrk <behcet.senturk@protonmail.com>
2020-09-19 15:33:54 +0300

    New changes

--- src/gazebo_mantis_dqlearn.py
+++ src/gazebo_mantis_dqlearn.py
@@ -18,24 +18,61 @@ import tf
 from sensor_msgs.msg import LaserScan
 from std_srvs.srv import Empty
 
+"""
+There are 3 different maze map in this packet
+After start one of them with launch file you have to edit this parameter
+for that maze.
+
+Options:
+maze1
+maze2
+maze3
+"""
+SELECT_MAP = "maze1"
+
 class AgentPosController():
-    # This class teleport agent when env.rest called
-    # Because we dont want the agent spawn same center place everytime
+    '''
+    This class control robot position
+    We teleport our agent when environment reset
+    So agent start from different position in every episode
+    '''
     def __init__(self):
         self.agent_model_name = "mantis"
 
     def teleportRandom(self):
-        # Teleport agent return new x and y point
-        model_state_msg = ModelState()
+        '''
+        Teleport agent return new x and y point
 
+        return agent posX, posY in list
+        '''
+
+        model_state_msg = ModelState()
         model_state_msg.model_name = self.agent_model_name
         
-        # stage 2
-        xy_list = [
-            [0,0], [0.5,0.0],
-            [-0.5,1.0], [-0.5,-1.0], [-1.5,0.5], [-1.5,-0.5],
-            [1.0,1.5], [-1.5,1.5], [1.0,-1.5], [-1.5,-1.5]
-        ]
+        if SELECT_MAP == "maze1":
+            # maze 1
+            xy_list = [
+                [-1.5, 0.5], [-1.5, 1.5], [-0.5, 0.5], [-0.5, 1.5],
+                [0.5, -0.5], [0.5, -1.5], [2.5, -0.5], [2.5, 0.5],
+                [5.5,-1.5], [5.5,-0.5], [5.5,0.5], [5.5,1.5]
+            ]
+        elif SELECT_MAP == "maze2":
+            # maze 2
+            xy_list = [
+                [-1.5,-1.5], [-0.5,-1.5], [-1.5,-0.5], 
+                [-0.5,1.5], [1.5,0.5], 
+                [2.5,2.5], [2.5,3.5], [1.5,3.5], 
+            ]
+        else:
+            # maze 3
+            xy_list = [
+                [0.5,0.5], [1.5,0.5], [0.5,1.5], [1.5,1.5],
+                [-0.5,-0.5], [-1.5,-0.5], [-1.5,-1.5],
+                [0.5,-0.5], [0.5,-1.5], [1.5,-1.5],
+                [-1.5,0.5], [-0.5,1.5], [-1.5,1.5],
+            ]
+        
+        # Get random position for agent
         pose = Pose()
         pose.position.x, pose.position.y = random.choice(xy_list)
 
@@ -44,6 +81,7 @@ class AgentPosController():
 
         model_state_msg.reference_frame = "world"
 
+        # Start teleporting in Gazebo
         isTeleportSuccess = False
         for i in range(5):
             if not isTeleportSuccess:
@@ -66,6 +104,9 @@ class AgentPosController():
         return pose.position.x, pose.position.y
 
 class GoalController():
+    """
+    This class controls target model and position
+    """
     def __init__(self):
         self.model_path = "../models/gazebo/goal_sign/model.sdf"
         f = open(self.model_path, 'r')
@@ -78,13 +119,15 @@ class GoalController():
         self.last_goal_y = self.goal_position.position.y
 
         self.model_name = 'goal_sign'
-        self.check_model = False
+        self.check_model = False  # This used to checking before spawn model if there is already a model
 
     def respawnModel(self):
-        # Spawn goal model
+        '''
+        Spawn model in Gazebo
+        '''
         isSpawnSuccess = False
         for i in range(5):
-            if not self.check_model:
+            if not self.check_model:  # This used to checking before spawn model if there is already a model
                 try:
                     rospy.wait_for_service('gazebo/spawn_sdf_model')
                     spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
@@ -103,7 +146,9 @@ class GoalController():
         
 
     def deleteModel(self):
-        # Delete goal model
+        '''
+        Delete model from Gazebo
+        '''
         while True:
             if self.check_model:
                 try:
@@ -118,43 +163,51 @@ class GoalController():
                 break
 
     def calcTargetPoint(self):
+        """
+        This function return a target point randomly for robot
+        """
         self.deleteModel()
-        
-        # stage 2
-        
-        goal1_x_list = [1.5, 0, -1.5]
-        goal1_y_list = [0.5, -0.5]
+        # Wait for deleting
+        time.sleep(0.5)
 
-        goal2_y_list = [1.5, 0, -1.5]
-        goal2_x_list = [0.5, -0.5]
+        if SELECT_MAP == "maze1":
+            # maze 1
+            goal_xy_list = [
+                [-1.5, 0.5], [-1.5, 1.5], [-0.5, 0.5], [-0.5, 1.5],
+                [0.5, -0.5], [0.5, -1.5], [2.5, -0.5], [2.5, 0.5],
+                [5.5,-1.5], [5.5,-0.5], [5.5,0.5], [5.5,1.5]
+            ]
+        elif SELECT_MAP == "maze2":
+            # maze 2
+            goal_xy_list = [
+                [-1.5,-1.5], [-0.5,-1.5], [-1.5,-0.5], 
+                [-0.5,1.5], [1.5,0.5], 
+                [2.5,2.5], [2.5,3.5], [1.5,3.5], 
+            ]
+        else:
+            # maze 3
+            goal_xy_list = [
+                [0.5,0.5], [1.5,0.5], [0.5,1.5], [1.5,1.5],
+                [-0.5,-0.5], [-1.5,-0.5], [-1.5,-1.5],
+                [0.5,-0.5], [0.5,-1.5], [1.5,-1.5],
+                [-1.5,0.5], [-0.5,1.5], [-1.5,1.5],
+            ]
         
-        # maze 2
-        '''     
-        goal1_x_list = [2.5, -2.5]
-        goal1_y_list = [2.5, 1.5, 0.5, -0.5, -1.5, -2.5]
-
-        goal2_y_list = [2.5, -2.5]
-        goal2_x_list = [2.5, 1.5, 0.5, -0.5, -1.5, -2.5]
-        '''
-
+        # Check last goal position not same with new goal
         while True:
-            if random.choice([True, False]):
-                self.goal_position.position.y = random.choice(goal2_y_list)
-                self.goal_position.position.x = random.choice(goal2_x_list)
-            else:
-                self.goal_position.position.y = random.choice(goal1_y_list)
-                self.goal_position.position.x = random.choice(goal1_x_list)
+            self.goal_position.position.x, self.goal_position.position.y = random.choice(goal_xy_list)
 
             if self.last_goal_x != self.goal_position.position.x:
                 if self.last_goal_y != self.goal_position.position.y:
                     break
 
-        time.sleep(0.5)
+        # Spawn goal model
         self.respawnModel()
 
         self.last_goal_x = self.goal_position.position.x
         self.last_goal_y = self.goal_position.position.y
 
+        # Inform user
         rospy.logwarn("New goal position : " + str(self.goal_position.position.x) + " , " + str(self.goal_position.position.y))
 
         return self.goal_position.position.x, self.goal_position.position.y
@@ -164,27 +217,32 @@ class GoalController():
 
 
 class MantisGymEnv():
-
+    '''
+    Main Gazebo environment class
+    Contains reset and step function
+    '''
     def __init__(self):
         # Initialize the node
         rospy.init_node('mantis_gym_env', anonymous=True)
+
         # Connect to gazebo
         self.velPub = rospy.Publisher('/diffdrive/cmd_vel', Twist, queue_size=5)
         self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
         self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
-        self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+        self.reset_proxy = rospy.ServiceProxy(
+            '/gazebo/reset_simulation', Empty)
 
         self.laserPointCount = 24  # 24 laser point in one time
-        self.minCrashRange = 0.3  # Asume crash below this distance
-        self.laserMinRange = 0.2
-        self.laserMaxRange = 10.0
+        self.minCrashRange = 0.2  # Asume crash below this distance
+        self.laserMinRange = 0.2  # Modify laser data and fix min range to
+        self.laserMaxRange = 10.0  # Modify laser data and fix max range to
         self.stateSize = self.laserPointCount + 4  # Laser(arr), heading, distance, obstacleMinRange, obstacleAngle
-        self.actionSize = 5
+        self.actionSize = 5  # Size of the robot's actions
 
         self.targetDistance = 0  # Distance to target
 
-        self.targetPointX = 0
-        self.targetPointY = 0
+        self.targetPointX = 0  # Target Pos X
+        self.targetPointY = 0  # Target Pos Y
 
         # Means robot reached target point. True at beginning to calc random point in reset func
         self.isTargetReached = True
@@ -193,7 +251,9 @@ class MantisGymEnv():
 
 
     def pauseGazebo(self):
-        # Pause the simulation
+        '''
+        Pause the simulation
+        '''
         rospy.wait_for_service('/gazebo/pause_physics')
         try:
             self.pause()
@@ -201,7 +261,9 @@ class MantisGymEnv():
             print("/gazebo/pause_physics service call failed")
 
     def unpauseGazebo(self):
-        # Unpause the simulation
+        '''
+        Unpause the simulation
+        '''
         rospy.wait_for_service('/gazebo/unpause_physics')
         try:
             self.unpause()
@@ -209,7 +271,9 @@ class MantisGymEnv():
             print("/gazebo/unpause_physics service call failed")
 
     def resetGazebo(self):
-        # Reset simualtion to initial phase
+        '''
+        Reset simualtion to initial phase
+        '''
         rospy.wait_for_service('/gazebo/reset_simulation')
         try:
             self.reset_proxy()
@@ -217,7 +281,11 @@ class MantisGymEnv():
             print("/gazebo/reset_simulation service call failed")
 
     def getLaserData(self):
-        # Return laser 2D array of robot scanner
+        '''
+        ROS callback function
+        
+        return laser scan in 2D list
+        '''
         try:
             laserData = rospy.wait_for_message('/mantis/base_scan', LaserScan, timeout=5)
             return laserData
@@ -225,9 +293,14 @@ class MantisGymEnv():
             rospy.logfatal("Error to get laser data " + str(e))
 
     def getOdomData(self):
-        # Return yaw, robotX, robotY of robot
+        '''
+        ROS callback function
+        Modify odom data quaternion to euler
+
+        return yaw, posX, posY of robot known as Pos2D
+        '''
         try:
-            odomData = rospy.wait_for_message('/diffdrive/odom', Odometry, timeout=5)
+            odomData = rospy.wait_for_message('/odom', Odometry, timeout=5)
             odomData = odomData.pose.pose
             quat = odomData.orientation
             quatTuple = (
@@ -241,11 +314,16 @@ class MantisGymEnv():
             robotX = odomData.position.x
             robotY = odomData.position.y
             return yaw, robotX, robotY
+
         except Exception as e:
             rospy.logfatal("Error to get odom data " + str(e))
 
     def calcHeadingAngle(self, targetPointX, targetPointY, yaw, robotX, robotY):
-        # Return heading angle from robot to target
+        '''
+        Calculate heading angle from robot to target
+
+        return angle in float
+        '''
         targetAngle = math.atan2(targetPointY - robotY, targetPointX - robotX)
 
         heading = targetAngle - yaw
@@ -258,17 +336,36 @@ class MantisGymEnv():
         return round(heading, 2)
 
     def calcDistance(self, x1, y1, x2, y2):
-        # Return euler distance of two point in 2D space
+        '''
+        Calculate euler distance of given two points
+
+        return distance in float
+        '''
         return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
     def calculateState(self, laserData, odomData):
-        # Calculate and return state
-        heading = self.calcHeadingAngle(self.targetPointX, self.targetPointY, *odomData)
+        '''
+        Modify laser data
+        Calculate heading angle
+        Calculate distance to target
+        Calculate min range to nearest obstacle
+        Calculate angle to nearest obstacle
+
+        returns state as np.array
+
+        State contains:
+        laserData, heading, distance, obstacleMinRange, obstacleAngle
+        '''
+
+        heading = self.calcHeadingAngle(
+            self.targetPointX, self.targetPointY, *odomData)
         _, robotX, robotY = odomData
-        distance = self.calcDistance(robotX, robotY, self.targetPointX, self.targetPointY)
+        distance = self.calcDistance(
+            robotX, robotY, self.targetPointX, self.targetPointY)
 
         isCrash = False  # If robot hit to an obstacle
         laserData = list(laserData.ranges)
+
         for i in range(len(laserData)):
             if (self.minCrashRange > laserData[i] > 0):
                 isCrash = True
@@ -283,6 +380,18 @@ class MantisGymEnv():
         return laserData + [heading, distance, obstacleMinRange, obstacleAngle], isCrash
 
     def step(self, action):
+        '''
+        Act in envrionment
+        After action return new state
+        Calculate reward
+        Calculate bot is crashed or not
+        Calculate is episode done or not
+
+        returns state as np.array
+
+        State contains:
+        laserData, heading, distance, obstacleMinRange, obstacleAngle, reward, done
+        '''
         self.unpauseGazebo()
 
         # Move
@@ -292,8 +401,10 @@ class MantisGymEnv():
         velCmd = Twist()
         velCmd.linear.x = 0.15
         velCmd.angular.z = angVel
+
         self.velPub.publish(velCmd)
 
+        # More basic actions
         """
         if action == 0: #BRAKE LEFT
             velCmd = Twist()
@@ -321,6 +432,7 @@ class MantisGymEnv():
             velCmd.angular.z = -1.6
             self.velPub.publish(velCmd)       
         """
+
         # Observe
         laserData = self.getLaserData()
         odomData = self.getOdomData()
@@ -339,19 +451,24 @@ class MantisGymEnv():
             self.isTargetReached = True
 
         if isCrash:
-            # Crashed
             reward = -150
+
         elif self.isTargetReached:
             # Reached to target
             rospy.logwarn("Reached to target!")
             reward = 200
+            # Calc new target point
             self.targetPointX, self.targetPointY = self.goalCont.calcTargetPoint()
             self.isTargetReached = False
+
         else:
-            # Normal Action
+            # Neither reached to goal nor crashed calc reward for action
             yawReward = []
             currentDistance = state[-3]
             heading = state[-4]
+
+            # Calc reward 
+            # reference https://emanual.robotis.com/docs/en/platform/turtlebot3/ros2_machine_learning/
 
             for i in range(self.actionSize):
                 angle = -math.pi / 4 + heading + (math.pi / 8 * i) + math.pi / 2
@@ -369,16 +486,24 @@ class MantisGymEnv():
         return np.asarray(state), reward, done
 
     def reset(self):
-        # Resets the state of the environment and returns an initial observation.
+        '''
+        Reset the envrionment
+        Reset bot position
+
+        returns state as np.array
+
+        State contains:
+        laserData, heading, distance, obstacleMinRange, obstacleAngle
+        '''
         self.resetGazebo()
 
         while True:
-            # Teleport mantis to a random point
+            # Teleport bot to a random point
             agentX, agentY = self.agentController.teleportRandom()
             if self.calcDistance(self.targetPointX, self.targetPointY, agentX, agentY) > self.minCrashRange:
                 break
             else:
-                rospy.logerr("Re teleporting the mantis!")
+                rospy.logerr("Reteleporting the bot!")
                 time.sleep(2)
 
         if self.isTargetReached:
@@ -388,7 +513,7 @@ class MantisGymEnv():
                     self.isTargetReached = False
                     break
                 else:
-                    rospy.logerr("Re calculating target point!")
+                    rospy.logerr("Recalculating the target point!")
                     time.sleep(2)
 
         # Unpause simulation to make observation
@@ -401,4 +526,4 @@ class MantisGymEnv():
         self.targetDistance = state[-3]
         self.stateSize = len(state)
 
-        return np.asarray(state)
+        return np.asarray(state)  # Return state
--- src/mantis_lidar_dqlearn.py
+++ src/mantis_lidar_dqlearn.py
@@ -16,8 +16,12 @@ import matplotlib.pyplot as plt
 import sys
 import signal
 
+plotDuringTrain = False  # Rise a new window to plot process while training
+
 class LivePlot():
-    # Live plot with matplotlib for epoch, score, inform text
+    '''
+    Class for live plot while training for epoch and score
+    '''
     def __init__(self):
         self.x = [0]
         self.y = [0]
@@ -38,43 +42,52 @@ class LivePlot():
         plt.clf()
 
 class Agent:
-
+    '''
+    Main class for agent
+    '''
     def __init__(self, stateSize, actionSize):
         self.useConvNet = False  # Use Conv1D network
-        self.isTrainActive = False  # Train model or just predict
-        self.loadModel = True  # Load model from file
-        self.loadEpisodeFrom = 6324  # Start to learn from this episode
+        self.isTrainActive = True  # Train model (Make it False for just testing)
+        self.loadModel = False  # Load model from file
+        self.loadEpisodeFrom = 0  # Start to learn from this episode
         self.episodeCount = 40000  # Total episodes
         self.stateSize = stateSize  # Step size get from env
         self.actionSize = actionSize  # Action size get from env
-        self.targetUpdateCount = 2000  # Update target model at every targetUpdateCount
+        self.targetUpdateCount = 2000  # Update target model at every targetUpdateCount step
         self.saveModelAtEvery = 2  # Save model at every saveModelAtEvery epoch
         self.discountFactor = 0.99  # For qVal calculations
         self.learningRate = 0.0003  # For model
         self.epsilon = 1.0  # Exploit or Explore?
-        self.epsilonDecay = 0.99  # epsilon will multiplicated with this thing in every epoch
+        self.epsilonDecay = 0.99  # To decay epsilon at every episode
         self.epsilonMin = 0.05  # Epsilon never fall more then this
         self.batchSize = 64  # Size of a miniBatch
-        self.learnStart = 64  # Start to train model from this step
+        self.learnStart = 100000  # Start to train model from this step
         self.memory = deque(maxlen=200000)  # Main memory to keep batchs
-        self.timeOutLim = 700  # After this end the epoch
+        self.timeOutLim = 1400  # After this step end the epoch
+        self.savePath = '/tmp/mantisModel/'  # Model save path
 
+        # Conv1D ne can be used
         if not self.useConvNet:
-            self.model = self.initNetwork()
+            self.onlineModel = self.initNetwork()
             self.targetModel = self.initNetwork()
         else:
-            self.model = self.initConvNetwork()
+            self.onlineModel = self.initConvNetwork()
             self.targetModel = self.initConvNetwork()
 
         self.updateTargetModel()
 
-        self.savePath = '/tmp/mantisModel/'
+        # Create model file path
         try:
             os.mkdir(self.savePath)
         except Exception:
             pass
 
     def initNetwork(self):
+        '''
+        Build DNN
+
+        return Keras DNN model
+        '''
         model = Sequential()
 
         model.add(Dense(64, input_shape=(self.stateSize,), activation="relu", kernel_initializer="lecun_uniform"))
@@ -87,6 +100,11 @@ class Agent:
         return model
 
     def initConvNetwork(self):
+        '''
+        Build CNN
+
+        return Keras CNN model
+        '''
         model = Sequential()
 
         model.add(Reshape((self.stateSize, 1), input_shape=(self.stateSize, )))
@@ -103,40 +121,52 @@ class Agent:
 
         return model
 
-    def calcQ(self, reward, nextStateQVal, done):
+    def calcQ(self, reward, nextTarget, done):
         """
-        QL:
-            Q(s, a) += alpha * (reward(s,a) + gamma * max(Q(s') - Q(s,a))
+        Calculates q value
+        target = reward(s,a) + gamma * max(Q(s')
 
-        DQL:
-            if not done:
-                target = reward(s,a) + gamma * max(Q(s'))
-            if done:
-                target = reward
-
+        return q value in float
         """
         if done:
             return reward
         else:
-            return reward + self.discountFactor * np.amax(nextStateQVal)
+            return reward + self.discountFactor * np.amax(nextTarget)
 
     def updateTargetModel(self):
-        self.targetModel.set_weights(self.model.get_weights())
+        '''
+        Update target model weights with online model weights
+        '''
+        self.targetModel.set_weights(self.onlineModel.get_weights())
 
     def calcAction(self, state):
-        # Return selected action
-        if np.random.rand() <= self.epsilon:
+        '''
+        Caculates an Action
+
+        returns action number in int
+        '''
+        
+        if np.random.rand() <= self.epsilon:  # return random action
             self.qValue = np.zeros(self.actionSize)
             return random.randrange(self.actionSize)
-        else:
-            qValue = self.model.predict(state.reshape(1, self.stateSize))
+        else:  # Ask action to neural net
+            qValue = self.onlineModel.predict(state.reshape(1, self.stateSize))
             self.qValue = qValue
             return np.argmax(qValue[0])
     
     def appendMemory(self, state, action, reward, nextState, done):
+        '''
+        Append state to replay mem
+        '''
         self.memory.append((state, action, reward, nextState, done))
 
     def trainModel(self, target=False):
+        '''
+        Train model with randomly choosen minibatches
+        Uses Double DQN
+        '''
+        
+        # Get minibatches
         miniBatch = random.sample(self.memory, self.batchSize)
         xBatch = np.empty((0, self.stateSize), dtype=np.float64)
         yBatch = np.empty((0, self.actionSize), dtype=np.float64)
@@ -148,50 +178,53 @@ class Agent:
             nextState = miniBatch[i][3]
             done = miniBatch[i][4]
 
-            qValue = self.model.predict(state.reshape(1, len(state)))
+            qValue = self.onlineModel.predict(state.reshape(1, len(state)))
             self.qValue = qValue
 
             if target:
-                nextStateQVal = self.targetModel.predict(nextState.reshape(1, len(nextState)))
-
+                nextTarget = self.targetModel.predict(nextState.reshape(1, len(nextState)))
             else:
-                nextStateQVal = self.model.predict(nextState.reshape(1, len(nextState)))
+                nextTarget = self.onlineModel.predict(nextState.reshape(1, len(nextState)))
 
-            targetTruth = self.calcQ(reward, nextStateQVal, done)
+            nextQValue = self.calcQ(reward, nextTarget, done)
 
             xBatch = np.append(xBatch, np.array([state.copy()]), axis=0)
             ySample = qValue.copy()
 
-            ySample[0][action] = targetTruth
+            ySample[0][action] = nextQValue
             yBatch = np.append(yBatch, np.array([ySample[0]]), axis=0)
 
             if done:
                 xBatch = np.append(xBatch, np.array([nextState.copy()]), axis=0)
                 yBatch = np.append(yBatch, np.array([[reward] * self.actionSize]), axis=0)
 
-        self.model.fit(xBatch, yBatch, batch_size=self.batchSize, epochs=1, verbose=0)
+        self.onlineModel.fit(xBatch, yBatch, batch_size=self.batchSize, epochs=1, verbose=0)
 
 
 if __name__ == '__main__':
-    #score_plot = LivePlot()
-    env = MantisGymEnv()
+    if plotDuringTrain:
+        score_plot = LivePlot()
 
+    env = MantisGymEnv()  # Create environment
+
+    # get action and state sizes
     stateSize = env.stateSize
     actionSize = env.actionSize
 
+    # Create an agent
     agent = Agent(stateSize, actionSize)
 
+    # Load model from file if needed
     if agent.loadModel:
-        agent.model.set_weights(load_model(agent.savePath+str(agent.loadEpisodeFrom)+".h5").get_weights())
+        agent.onlineModel.set_weights(load_model(agent.savePath+str(agent.loadEpisodeFrom)+".h5").get_weights())
 
         with open(agent.savePath+str(agent.loadEpisodeFrom)+'.json') as outfile:
             param = json.load(outfile)
             agent.epsilon = param.get('epsilon')
 
+
     stepCounter = 0
-
     startTime = time.time()
-
     for epoch in range(agent.loadEpisodeFrom + 1, agent.episodeCount):
         done = False
         state = env.reset()
@@ -205,8 +238,7 @@ if __name__ == '__main__':
                 print("Error Score is too high or too low! Resetting...")
                 break
 
-            if agent.isTrainActive:
-                agent.appendMemory(state, action, reward, nextState, done)
+            agent.appendMemory(state, action, reward, nextState, done)
 
             if agent.isTrainActive and len(agent.memory) >= agent.learnStart:
                 if stepCounter <= agent.targetUpdateCount:
@@ -222,13 +254,15 @@ if __name__ == '__main__':
             action_text = "Action:{:.2f}  | ".format(action)
 
             inform_text = avg_max_q_val_text + reward_text + action_text
-
-            #score_plot.update(epoch, score, "Score", inform_text, updtScore=False)
             
-            if epoch % agent.saveModelAtEvery == 0:
+            if plotDuringTrain:
+                score_plot.update(epoch, score, "Score", inform_text, updtScore=False)
+            
+            # Save model to file
+            if agent.isTrainActive and epoch % agent.saveModelAtEvery == 0:
                 weightsPath = agent.savePath + str(epoch) + '.h5'
                 paramPath = agent.savePath + str(epoch) + '.json'
-                agent.model.save(weightsPath)
+                agent.onlineModel.save(weightsPath)
                 with open(paramPath, 'w') as outfile:
                     json.dump(paramDictionary, outfile)
 
@@ -238,11 +272,15 @@ if __name__ == '__main__':
 
             if done:
                 agent.updateTargetModel()
+
+                # Infor user
                 m, s = divmod(int(time.time() - startTime), 60)
                 h, m = divmod(m, 60)
 
                 print('Ep: {} | AvgMaxQVal: {:.2f} | CScore: {:.2f} | Mem: {} | Epsilon: {:.2f} | Time: {}:{}:{}'.format(epoch, np.max(agent.qValue), score, len(agent.memory), agent.epsilon, h, m, s))
-                #score_plot.update(epoch, score, "Score", inform_text, updtScore=True)
+                
+                if plotDuringTrain:
+                    score_plot.update(epoch, score, "Score", inform_text, updtScore=True)
 
                 paramKeys = ['epsilon']
                 paramValues = [agent.epsilon]
@@ -251,13 +289,9 @@ if __name__ == '__main__':
 
             stepCounter += 1
             if stepCounter % agent.targetUpdateCount == 0:
-                print("UPDATE TARGET NETWORK???")
+                agent.updateTargetModel()
 
+        # Epsilon decay
         if agent.epsilon > agent.epsilonMin:
             agent.epsilon *= agent.epsilonDecay
 
-"""
-Problem 1 = TragetUpdate ne olaki neden sadece print var
-Problem 2 = Done olunca qValue reward * actionSize oluyor. Ben hedefe ulasinca da done true ettigim icin bir sorun olur mu ki
-Problem 3 = neden max q sadece secilen aksiyon ile degistiriliyor
-"""
